4779c2f1d18752531df49bc2c38a5a2a3c0a4d9e
Joseph <joehms22@gmail.com>
2014-10-17 15:53:30 -0400

    added ability to choose port from cli

--- start.py
+++ start.py
@@ -1,22 +1,9 @@
 #!/usr/bin/env python
-
-import keyboard
-import webbrowser
-import os
-import time
 import cvserver
-import threading
-
-
-def startbrowser():
-    time.sleep(5)
-    path =  "file://" + os.path.abspath("index.html")    
-    webbrowser.open(path)
-    time.sleep(3)    
-    keyboard.fullscreen()
+import sys
 
 if __name__ == "__main__":
-
-    #t = threading.Thread(target = startbrowser)
-    #t.start()
-    cvserver.main()
+    port = 8080
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    cvserver.main(port)
