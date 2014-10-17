817939c748869a4786cb646766a9eb40590ee132
Daniel Kumor <daniel@dkumor.com>
2014-10-17 14:58:45 -0400

    Ripped out camera code
    Starting new better implementation

--- cvserver.py
+++ cvserver.py
@@ -3,10 +3,9 @@
     Adapted from simple mjpeg stream server (Igor Maculan - n3wtron@gmail.com)
 
 '''
-import cv2
-from PIL import Image
+
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-import StringIO
+from lawsoncam import LawsonCamera
 import urllib
 import numpy as np
 import time
@@ -35,11 +34,8 @@ class CamHandler(BaseHTTPRequestHandler):
         global rect
         global speed
         if self.path.endswith('.mjpg'):
-            stream=urllib.urlopen('http://128.10.29.32/mjpg/1/video.mjpg')
-            bytes=''
-            print "Videostream open"
-            i = None
-            i_prev = None
+            
+            cam = LawsonCamera()
 
             reset = False
             self.send_response(200)
@@ -49,73 +45,12 @@ class CamHandler(BaseHTTPRequestHandler):
             while True:
                 try:
 
-                    #HERE IS MONSTER CODE
-                    bytes+=stream.read(1024)
-                    a = bytes.find('\xff\xd8')
-                    b = bytes.find('\xff\xd9')
-                    if a!=-1 and b!=-1:
-
-                        jpg = bytes[a:b+2]
-                        bytes= bytes[b+2:]
-                        if (i_prev==None):
-                            #i = cv2.cvtColor(cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR), cv2.COLOR_RGB2GRAY)
-                            i =cv2.cvtColor(cv2.resize(cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR),(0,0),fx=0.5,fy=0.5), cv2.COLOR_RGB2GRAY)
-                            i = cv2.flip(i,1)
-                            i_prev = i
-                        else:
-                            i_prev = i
-                            #i = cv2.cvtColor(cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR), cv2.COLOR_RGB2GRAY)
-                            i =cv2.cvtColor(cv2.resize(cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_COLOR),(0,0),fx=0.5,fy=0.5), cv2.COLOR_RGB2GRAY)
-                            i=cv2.flip(i,1)
-                        #size:384,512
-                        #Grid: 128 down 170 across
-                        d = cv2.absdiff(i,i_prev)
-
-                        pk = None
-                        for k in rect:
-                            p = rect[k]
-                            
-                            diff = d[p[0][1]:p[1][1],p[0][0]:p[1][0]].sum()
-                            p[2]+= d[p[0][1]:p[1][1],p[0][0]:p[1][0]].sum()/(speed+equalizer*p[3])
-                            if (p[2] > 1.0):
-                                p[2]= 1.0
-                                if k == "up":
-                                    keyboard.up()
-                                elif k == "down":
-                                    keyboard.down()
-                                elif k == "left":
-                                    keyboard.left()
-                                elif k == "right":
-                                    keyboard.right()
-                                for z in rect:
-                                    if (z!=k):
-                                        rect[z][3]-=1
-                                        if (rect[z][3]<=0):
-                                            rect[z][3] = 0
-                                p[3]+=3
-                                logging.warning(str(k)+" "+str(p[3]))
-                                reset = True
-                                break
-
-                            cv2.rectangle(i,p[0],p[1],int(255*p[2]),3)
-                        if (reset):
-                            reset= False
-                            minval = rect["up"][3]
-                            for k in rect:
-                                rect[k][2] = 0.0
-                                if (rect[k][3] < minval):
-                                    minval = rect[k][3]
-                            for k in rect:
-                                rect[k][3] -= minval
-
-                        jpg = Image.fromarray(cv2.cvtColor(i,cv2.COLOR_GRAY2RGB))
-                        tmpFile = StringIO.StringIO()
-                        jpg.save(tmpFile,'JPEG')
-                        self.wfile.write("--jpgboundary")
-                        self.send_header('Content-type','image/jpeg')
-                        self.send_header('Content-length',str(tmpFile.len))
-                        self.end_headers()
-                        jpg.save(self.wfile,'JPEG')
+                    jpg = cam.jpgstream()
+                    self.wfile.write("--jpgboundary")
+                    self.send_header('Content-type','image/jpeg')
+                    self.send_header('Content-length',len(jpg))
+                    self.end_headers()
+                    self.wfile.write(jpg)
 
                 except KeyboardInterrupt:
                     break
---
+++ lawsoncam.py
@@ -0,0 +1,37 @@
+import cv2
+from PIL import Image
+from cStringIO import StringIO
+
+class LawsonCamera(object):
+    """Given box locations of different keys, allows to use the webcam as a controller"""
+    def __init__(self,address="rtsp://128.10.29.32:554/axis-media/media.amp?videocodec=h264&amp;camera=1&amp;streamprofile=Bandwidth"):
+        self.stream = cv2.VideoCapture(address)
+        self.keys = {}
+    def getFrame(self):
+        ret, frame = self.stream.read()
+        return frame
+    
+    def getLawsonFrame(self):
+        #Lawson's cameras have a black strip above video feed - remove the strip
+        return self.getFrame()[24:]
+
+    def __call__(self,retjpeg=False):
+        return self.getLawsonFrame()
+    def jpgstream(self):
+        img = Image.fromarray(self())
+        jpg = StringIO()
+        img.save(jpg,'JPEG')
+        return jpg.getvalue()
+
+
+        
+        
+
+if (__name__=="__main__"):
+    c = LawsonCamera()
+    
+    while (1):
+        img = c()
+        cv2.imshow('VIDEO', img)
+        if (cv2.waitKey(1)>0):
+            break
